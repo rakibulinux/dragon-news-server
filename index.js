220b1fe66b8fb719a4cc7ca27949e38706baf22b
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2022-10-22 15:11:42 +0600

    all done

--- index.js
+++ index.js
@@ -17,8 +17,24 @@ app.get("/categories", (req, res) => {
   res.send(cagegories);
 });
 
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
 app.get("/news", (req, res) => {
   res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 app.listen(port, () => {
   console.log("Dragon is running");
